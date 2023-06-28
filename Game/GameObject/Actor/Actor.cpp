72fe12bf3767f3d01fd2ec43eaf6c597690a16f8
momiji-anko <118102410+momiji-anko@users.noreply.github.com>
2023-06-28 22:51:12 +0900

    少し更新

--- Game/GameObject/Actor/Actor.cpp
+++ Game/GameObject/Actor/Actor.cpp
@@ -3,6 +3,33 @@
 
 #include"pch.h"
 
+/// <summary>
+/// 初期化	
+/// </summary>
+/// <param name="position">座標</param>
+/// <param name="velcity">移動量</param>
+/// <param name="scale">拡縮</param>
+/// <param name="rotataion">スケール</param>
+/// <param name="model">モデル</param>
+/// <param name="active">アクティブ</param>
+void Actor::Initialize(const DirectX::SimpleMath::Vector3& position, const DirectX::SimpleMath::Vector3& velcity, const DirectX::SimpleMath::Vector3& scale, const DirectX::SimpleMath::Vector3& rotataion, DirectX::Model* model, bool active)
+{
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+/// <param name="timer">タイマー</param>
+void Actor::Update(const DX::StepTimer& timer)
+{
+}
+
+/// <summary>
+/// 影生成
+/// </summary>
+/// <param name="shadow">シャドウマップの生ポインタ</param>
+/// <param name="view">ビュー行列</param>
+/// <param name="projection">プロジェクション行列</param>
 void Actor::CreateShadow(ShadowMap* shadow, const DirectX::SimpleMath::Matrix& view, const DirectX::SimpleMath::Matrix& projection)
 {
 }
